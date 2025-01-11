73fec6a488a7520879feb11f264d42957c374b30
Joseph Olapade <Olapade.j@yahoo.com>
2025-01-11 14:07:43 +0100

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Josev, another one!'));
+app.get('/', (req, res) => res.send('Hello Josev, another one. i think these time we did it all on our own!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
